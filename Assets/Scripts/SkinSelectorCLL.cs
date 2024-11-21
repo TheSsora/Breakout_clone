f858e0e7ee5b76e1f13c77fe991eff70b08362b8
Artem <52154996+TheSsora@users.noreply.github.com>
2024-11-21 12:54:33 +0300

    0.4
    -Системы выбора скина платформы

---
+++ Assets/Scripts/SkinSelectorCLL.cs
@@ -0,0 +1,73 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
+using UnityEditor;
+using UnityEngine;
+using UnityEngine.EventSystems;
+using UnityEngine.UI;
+
+public class SkinSelectorCLL : MonoBehaviour, IPointerClickHandler
+{
+    [SerializeField] int ID;
+    [SerializeField] GeneralGameData gameData;
+    [SerializeField] Image skinSprite;
+    [SerializeField] Image closedTexture;
+
+    [SerializeField] Image iconUI;
+    [SerializeField] Sprite iconSpriteSelected;
+    [SerializeField] Sprite iconSpriteDeselected;
+
+    private PlatformSkin platformSkin;
+
+    public void OnPointerClick(PointerEventData eventData)
+    {
+        if (platformSkin != null)
+        {
+            if(platformSkin.IsOpen && !platformSkin.IsSelected)
+            {
+                gameData.PlatformSkins.FirstOrDefault(x => x.IsSelected).IsSelected = false;
+
+                platformSkin.IsSelected = true;
+            }
+        }
+        transform.parent.GetComponentsInChildren<SkinSelectorCLL>().ToList().ForEach(x => x.UpdateUI());
+        //LayoutRebuilder.ForceRebuildLayoutImmediate(transform.parent.GetComponent<RectTransform>());
+        //UpdateUI();
+    }    
+    private void OnEnable()
+    {
+        if (ID < gameData.PlatformSkins.Count)
+        {
+            platformSkin = gameData.PlatformSkins[ID];            
+        }
+
+        UpdateUI();
+    }    
+    private void OnValidate()
+    {
+        if(ID < gameData.PlatformSkins.Count)
+        {
+            platformSkin = gameData.PlatformSkins[ID];            
+        }
+
+        UpdateUI();
+    }
+    
+    public void UpdateUI()
+    {        
+        if(platformSkin != null)
+        {
+            skinSprite.sprite = platformSkin.sprite;
+            closedTexture.gameObject.SetActive(!platformSkin.IsOpen);
+            if(platformSkin.IsSelected)
+            {
+                iconUI.sprite = iconSpriteSelected;
+            }
+            else
+            {
+                iconUI.sprite = iconSpriteDeselected;
+            }            
+        }               
+    }
+}
